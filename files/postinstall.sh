5fbeceddd83ed0963c2b1b7b73b7898329cc5bde
Mick Pollard <aussielunix@gmail.com>
2015-11-23 09:24:05 +1100

    changes

--- files/postinstall.sh
+++ files/postinstall.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 pip install ansible
+
+echo "#!/bin/bash" > /home/lunix/getstarted.sh
+echo "" >> /home/lunix/getstarted.sh
+echo "git clone git@bitbucket.org:aussielunix/ansible-laptop.git" >> /home/lunix/getstarted.sh
+echo "cd ~/.ansible" >> /home/lunix/getstarted.sh
+echo 'eval "$(ssh-agent)"' >> /home/lunix/getstarted.sh
+
