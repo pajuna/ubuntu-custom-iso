77fc2456f5080fb05afac214f2abb2aa4053aa3b
Mick Pollard <aussielunix@gmail.com>
2015-11-29 18:39:59 +1100

    use expanded dirs for $HOME

--- files/postinstall.sh
+++ files/postinstall.sh
@@ -3,8 +3,16 @@
 pip install ansible
 
 echo "#!/bin/bash" > /home/lunix/getstarted.sh
-echo "" >> /home/lunix/getstarted.sh
-echo "git clone git@bitbucket.org:aussielunix/ansible-laptop.git" >> /home/lunix/getstarted.sh
-echo "cd ~/.ansible" >> /home/lunix/getstarted.sh
+echo ""  >> /home/lunix/getstarted.sh
+echo "mkdir /home/lunix/.ssh"  >> /home/lunix/getstarted.sh
+echo "cp -a * /home/lunix/.ssh/"  >> /home/lunix/getstarted.sh
+echo "chown -R lunix:lunix /home/lunix/.ssh"  >> /home/lunix/getstarted.sh
+echo "chmod -R 600 /home/lunix/.ssh"  >> /home/lunix/getstarted.sh
+echo "chmod 700 /home/lunix/.ssh"  >> /home/lunix/getstarted.sh
+echo ""  >> /home/lunix/getstarted.sh
 echo 'eval "$(ssh-agent)"' >> /home/lunix/getstarted.sh
+echo "ssh-add"  >> /home/lunix/getstarted.sh
+echo "" >> /home/lunix/getstarted.sh
+echo "git clone git@bitbucket.org:aussielunix/ansible-laptop.git /home/lunix/.ansible" >> /home/lunix/getstarted.sh
+echo "cd /home/lunix/.ansible" >> /home/lunix/getstarted.sh
 
